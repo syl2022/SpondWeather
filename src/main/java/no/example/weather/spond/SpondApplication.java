037ccead7ac8ab0c853ca927df06eb9cbcc47aa5
Shruti Yadav <shruti.yadav@turtlesec.no>
2024-08-10 14:21:15 +0200

    first commit

---
+++ src/main/java/no/example/weather/spond/SpondApplication.java
@@ -0,0 +1,13 @@
+package no.example.weather.spond;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpondApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpondApplication.class, args);
+	}
+
+}
---
+++ src/test/java/no/example/weather/spond/SpondApplicationTests.java
@@ -0,0 +1,13 @@
+package no.example.weather.spond;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpondApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
